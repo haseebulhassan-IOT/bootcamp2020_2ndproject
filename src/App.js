321cb5d57c62e0f0380b9510b0be72cb1786a90f
Haseeb ul Hassan <haseeb.ee12@gmail.com>
2020-06-06 01:35:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>Welcome to React App</h1>
       <h2> This is created by Haseeb Ul Hassan</h2>
-      <h3> 2nd project of Bootcamp 2020 done!!</h3>
+      <h3> 2nd project of Bootcamp 2020 done!!!</h3>
     </div>
   );
 }
