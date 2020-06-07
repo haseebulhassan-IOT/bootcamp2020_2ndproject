9da4378fffe1b2aabcf0ce507cc1abebc659cbfb
Haseeb ul Hassan <haseeb.ee12@gmail.com>
2020-06-07 12:34:30 +0500

    Changed the background color
    I have changed the background color of Welcome to React App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import './hello.css';
 
 function App({name}) {
   return (
     <div className="App">
-      <h1>Welcome to React App {name}</h1>
+      <h1 className = "myname">Welcome to React App {name}</h1>
       <h2> Hello from Haseeb Ul Hassan</h2>
-      <h3> This is created by Haseeb Ul Hassan</h3>
+      <p> This is created by <strong>Haseeb Ul Hassan</strong></p>
       <h4> 2nd project of Bootcamp 2020 done!!!</h4> 
       <div> Age is 26</div>
     </div>
