208dc7eff95d5cb0528376fba1fec727a8f0c4e9
Haseeb ul Hassan <haseeb.ee12@gmail.com>
2020-06-07 11:52:31 +0500

    Updated app.js and index.js
    I have added the properties to show my name in the website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App(name) {
   return (
     <div className="App">
-      <h1>Welcome to React App</h1>
+      <h1>Welcome to React App {name}</h1>
       <h2> Hello from Haseeb Ul Hassan</h2>
       <h3> This is created by Haseeb Ul Hassan</h3>
-      <h4> 2nd project of Bootcamp 2020 done!!!</h4>
+      <h4> 2nd project of Bootcamp 2020 done!!!</h4> 
+      <div> Age is 26</div>
     </div>
   );
 }
